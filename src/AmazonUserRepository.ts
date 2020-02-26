005db779def7c5f8e8822b82e5fe6fa0d16412ff
Volker Christian <volker.christian@fh-hagenberg.at>
2020-02-26 13:09:12 +0100

    Fixes

--- src/AmazonUserRepository.ts
+++ src/AmazonUserRepository.ts
@@ -1,72 +1,49 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, DeleteResult } from 'typeorm';
 import { AmazonUser } from './entity/AmazonUser';
 import { AmazonApiEndpoint } from './entity/AmazonApiEndpoint';
 
 @EntityRepository(AmazonUser)
 export class AmazonUserRepository extends Repository<AmazonUser> {
     getUserWithProactiveEndpoint(userId: string, skillId: string) {
-        return new Promise<AmazonUser>((resolve, reject) => {
-            this.createQueryBuilder('user')
-                .leftJoinAndMapOne(
-                    'user.amazonApiEndpoint',
-                    AmazonApiEndpoint, "amazonApiEndpoint",
-                    "amazonApiEndpoint.id = user.amazonApiEndpointId"
-                )
-                .where(
-                    'user.userId = :userId', { userId: userId }
-                )
-                .andWhere(
-                    "amazonApiEndpoint.applicationId = :skillId", { skillId: skillId }
-                )
-                .getOne()
-                .then((user) => resolve(user))
-                .catch((reason) => reject(reason))
-        });
+        return this.createQueryBuilder('user')
+            .leftJoinAndMapOne(
+                'user.amazonApiEndpoint',
+                AmazonApiEndpoint, "amazonApiEndpoint",
+                "amazonApiEndpoint.id = user.amazonApiEndpointId"
+            )
+            .where(
+                'user.userId = :userId', { userId: userId }
+            )
+            .andWhere(
+                "amazonApiEndpoint.applicationId = :skillId", { skillId: skillId }
+            )
+            .getOne();
     }
 
-    
+
     getUser(userId: string) {
-        return new Promise<AmazonUser>((resolve, reject) => {
-            this.createQueryBuilder('user')
-                .leftJoinAndMapOne(
-                    'user.amazonApiEndpoint',
-                    AmazonApiEndpoint, "amazonApiEndpoint",
-                    "amazonApiEndpoint.id = user.amazonApiEndpointId"
-                )
-                .where(
-                    'user.userId = :userId', { userId: userId }
-                )
-                .getOne()
-                .then((user) => resolve(user))
-                .catch((reason) => reject(reason))
-        });
+        return this.createQueryBuilder('user')
+            .leftJoinAndMapOne(
+                'user.amazonApiEndpoint',
+                AmazonApiEndpoint, "amazonApiEndpoint",
+                "amazonApiEndpoint.id = user.amazonApiEndpointId"
+            )
+            .where(
+                'user.userId = :userId', { userId: userId }
+            )
+            .getOne();
     }
 
 
-    /*
-    delete(userId: string, skillId: string) {
-        new Promise<DeleteResult>((resolve, reject) => {
-            this.createQueryBuilder()
-                .delete()
-                .where(
-                    'userId = :userId', { userId: userId }
-                )
-                .andWhere(
-                    'applicationId = :skillId', { skillId: skillId }
-                )
-                .execute()
-                .then(value => resolve(value))
-                .catch(reason => resolve(reason));
-        })
+    deleteUser(userId: string, skillId: string) {
+        return this.createQueryBuilder()
+            .delete()
+            .where(
+                'userId = :userId', { userId: userId }
+            )
+            .andWhere(
+                'applicationId = :skillId', { skillId: skillId }
+            )
+            .execute();
     }
-    */
-    /*
-        static save(user: AmazonUser) {
-            new Promise<AmazonUser>((resolve, reject) => {
-                save(user)
-                    .then(user => resolve(user))
-                    .catch(reason => reject(reason));
-            });
-        }
-    */
 }
